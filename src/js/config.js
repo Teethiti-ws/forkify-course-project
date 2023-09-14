0e45006c5d3265d8204a5f1042a5fb61cf217af6
Teethiti <Teethiti.ws@gmail.com>
2023-09-15 00:37:34 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 5;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bee33ed2-a11d-4667-b850-2357bd8cf4a9';
+export const MODAL_CLOSE_SEC = 2.5;
